b20b466a399acff57ae90da73b805a80d3699eb7
Hana <hana@argonet.co.kr>
2022-07-18 18:12:21 +0900

    init

---
+++ src/main/java/com/wdtt/LoggetherApplication.java
@@ -0,0 +1,13 @@
+package com.wdtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggetherApplication.class, args);
+	}
+
+}
