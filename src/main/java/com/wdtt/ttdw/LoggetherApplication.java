154b342a655aa96939dacc508aec3104729ff496
Hana <hana@argonet.co.kr>
2022-08-17 14:56:32 +0900

    중복 애노테이션 삭제

--- src/main/java/com/wdtt/ttdw/LoggetherApplication.java
+++ src/main/java/com/wdtt/ttdw/LoggetherApplication.java
@@ -3,12 +3,9 @@ package com.wdtt.ttdw;
 import javax.servlet.ServletContext;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -22,8 +19,6 @@ import com.mitchellbosecke.pebble.spring4.PebbleViewResolver;
 import com.mitchellbosecke.pebble.spring4.extension.SpringExtension;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan("com.wdtt.ttdw")
 @EntityScan("com.wdtt.ttdw.entity")
 @EnableJpaRepositories("com.wdtt.ttdw.entity.repository")
 @EnableJpaAuditing
